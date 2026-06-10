b59187ba651bff3a0e60b33875cd2454147117f0
Ajay-Kumar-13 <apulleti@gitam.in>
2026-06-10 18:52:11 +0000

    Sync LeetCode submission - Boats to Save People (python)

---
+++ solutions/problems/boats_to_save_people/solution.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        """
+        :type people: List[int]
+        :type limit: int
+        :rtype: int
+        """
+        people.sort()
+
+
+        i = 0
+        j = len(people) - 1
+        count = 0
+
+        while i <= j:
+
+            s = people[i] + people[j]
+
+            if i == j:
+                i += 1
+            elif s <= limit:
+                i += 1
+                j -= 1
+            else:
+                if people[i] > people[j]:
+                    i += 1
+                else:
+                    j -= 1
+            
+            count += 1
+        
+        return count
