9a0354041951b28b26876d2f3ab49c701c4690e1
Ajay-Kumar-13 <apulleti@gitam.in>
2026-06-28 15:01:35 +0000

    Sync LeetCode submission - 3Sum With Multiplicity (python)

--- solutions/problems/3sum_with_multiplicity/solution.py
+++ solutions/problems/3sum_with_multiplicity/solution.py
@@ -5,58 +5,54 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        frequency = {}
-        total = 0
-
-        def total_combinations(n, r):
-            if r > n:
+        
+        def possibleCombinations(n, r):
+            if n < r:
                 return 0
-
+                
             numerator = 1
             denominator = 1
-
+            
             for i in range(r):
-                numerator *= (n - i)
-                denominator *= (i + 1)
-
+                numerator = numerator * (n-i)
+                denominator = denominator * (i+1)
+                
             return numerator // denominator
+        
+        count = 0
+        
+        frequency = {}
 
-
-        for x in arr:
-            frequency[x] = frequency.get(x, 0) + 1
-
-        arr = sorted(set(arr))
-
+        for ele in arr:
+            frequency[ele] = frequency.get(ele, 0)+1
+            
+        
+        arr = sorted(list(set(arr)))
+        
         for i in range(len(arr)):
-            for j in range(i, len(arr)):
-                k = target - arr[i] - arr[j]
+            k = len(arr) - 1
+            j = i
+            while j <= k:
+                
+                t = target - arr[j] - arr[k]
 
-                if k not in frequency:
-                    continue
-
-                if k < arr[j]:
-                    continue
-
-                if arr[i] == arr[j] == k:
-                    total += total_combinations(frequency[arr[i]], 3)
-
-                elif arr[i] == arr[j]:
-                    total += (
-                        total_combinations(frequency[arr[i]], 2)
-                        * frequency[k]
-                    )
-
-                elif arr[j] == k:
-                    total += (
-                        frequency[arr[i]]
-                        * total_combinations(frequency[arr[j]], 2)
-                    )
-
+                if t == arr[i]:
+                    
+                    if arr[i] == arr[j] and arr[j] == arr[k]:
+                        count += possibleCombinations(frequency.get(arr[j]), 3)
+                    elif arr[j] == arr[k]:
+                        count += possibleCombinations(frequency.get(arr[j]), 2) * frequency.get(arr[i])
+                    elif arr[i] == arr[j]:
+                        count += frequency.get(arr[k]) * possibleCombinations(frequency.get(arr[i]), 2)
+                    else:
+                        count += frequency.get(arr[i]) * frequency.get(arr[j]) * frequency.get(arr[k])                    
+                    
+                    j += 1
+                    k -= 1
+                elif t < arr[i]:
+                    k -= 1
                 else:
-                    total += (
-                        frequency[arr[i]]
-                        * frequency[arr[j]]
-                        * frequency[k]
-                    )
+                    j += 1
+                    
 
-        return total % (10**9 + 7)
+        return count % 1000000007
