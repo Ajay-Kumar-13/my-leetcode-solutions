4f40c85067aad16b7f8b5745b55af1b1fdbe60df
Ajay-Kumar-13 <apulleti@gitam.in>
2026-06-28 17:06:43 +0000

    Sync LeetCode submission - Next Permutation (python)

--- solutions/problems/next_permutation/solution.py
+++ solutions/problems/next_permutation/solution.py
@@ -4,43 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-
-        if len(nums) < 2:
-            return nums
         
-        i = len(nums) - 1
+        j = len(nums) - 1
 
-        while nums[i-1] >= nums[i] and i > 0:
-            i -= 1
+        while j > 0 and nums[j] <= nums[j-1]:
+            j -= 1
 
-        if i == 0:
+        if j == 0:
             nums.sort()
-            return nums
         else:
-            i -= 1
-            j = i + 1
+            i = len(nums) - 1
+            while nums[i] <= nums[j-1] and i > 0:
+                i -= 1
 
-            swap = j
+            nums[j-1], nums[i] = nums[i], nums[j-1]
+            nums[j:] = reversed(nums[j:])
+    
 
-            diff = float('inf')
-
-            while j < len(nums):
-
-                d = nums[j] - nums[i]
-
-                if diff >= d and d > 0:
-                    swap = j
-                    diff = d
-
-                j = j+ 1
-
-            nums[i], nums[swap] = nums[swap], nums[i] 
-            
-            j = len(nums)-1
-            i += 1
-            while i < j:
-                nums[i], nums[j] = nums[j], nums[i]
-                i += 1
-                j -= 1
-            
         return nums
