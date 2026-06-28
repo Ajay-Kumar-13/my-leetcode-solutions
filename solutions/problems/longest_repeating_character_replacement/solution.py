30f2b39a346606a3382c693531611e7ea2efc1e6
Ajay-Kumar-13 <apulleti@gitam.in>
2026-06-28 18:42:06 +0000

    Sync LeetCode submission - Longest Repeating Character Replacement (python)

--- solutions/problems/longest_repeating_character_replacement/solution.py
+++ solutions/problems/longest_repeating_character_replacement/solution.py
@@ -1,33 +1,28 @@
 class Solution(object):
     def characterReplacement(self, s, k):
         """
-        :type s: str
-        :type k: int
+        :type s: str        :type k: int
         :rtype: int
         """
-
-        if len(s) < 1:
-            return 1
         
         i = 0
         j = 0
-
-        frequency = {}
-        maxFrequency = 0
-        maxLength = 0
-
+        window = {}
+        
+        longestSubstringLength = 0
+        maxFreq = 0
+        
         while j < len(s):
-            window = j - i + 1
-            frequency[s[j]] = frequency.get(s[j], 0) + 1
-            maxFrequency = max(frequency.get(s[j]), maxFrequency)
-
-            if (window - maxFrequency) <= k:
-                maxLength = max(maxLength, window)
-            else:
-                frequency[s[i]] = frequency.get(s[i]) - 1
-                i += 1
-                # maxFrequency = max(list(frequency.values()))
+            window[s[j]] = window.get(s[j],0)+1
+            maxFreq = max(maxFreq, window.get(s[j]))
+            window_length = j - i + 1
             
+            while window_length - maxFreq > k:
+                window[s[i]] = window.get(s[i],0)-1
+                i += 1
+                window_length = j - i + 1
+                
+            longestSubstringLength = max(longestSubstringLength, window_length)
             j += 1
-
-        return maxLength
+            
+        return longestSubstringLength
