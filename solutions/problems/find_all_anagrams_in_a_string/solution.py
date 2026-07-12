23dac11189aaa3a21556141487672aeb8001a396
Ajay-Kumar-13 <apulleti@gitam.in>
2026-07-12 12:36:26 +0000

    Sync LeetCode submission - Find All Anagrams in a String (python)

--- solutions/problems/find_all_anagrams_in_a_string/solution.py
+++ solutions/problems/find_all_anagrams_in_a_string/solution.py
@@ -5,44 +5,48 @@ class Solution(object):
         :type p: str
         :rtype: List[int]
         """
+
         if len(p) > len(s):
             return []
 
-        window_length = len(p)
+        p_dict = {}
 
-        requirement = {}
+        for element in p:
+            p_dict[element] = p_dict.get(element, 0)+1
+
+        requirement = len(p_dict)
+
         window = {}
-        
-        s = list(s)
-
-        for x in p:
-            requirement[x] = requirement.get(x, 0) + 1
-
-        ans = []
 
         formed = 0
 
-        for i in range(len(p)):
-            window[s[i]] = window.get(s[i], 0)+1
-            if window.get(s[i]) == requirement.get(s[i]):
+        ans = []
+
+        for _ in range(len(p)):
+            val = s[_]
+            window[val] = window.get(val, 0)+1
+            if p_dict.get(val) == window.get(val):
                 formed += 1
-            if formed == len(requirement):
+            if formed == requirement:
                 ans.append(0)
 
         i = 1
         j = len(p)
 
         while j < len(s):
-            if requirement.get(s[i-1], 0) == window.get(s[i-1], 0):
+            if window.get(s[i-1], 0) == p_dict.get(s[i-1], 0) and window.get(s[i-1], 0) - 1 < p_dict.get(s[i-1], 0) and formed > 0:
                 formed -= 1
-            window[s[i-1]] = window.get(s[i-1]) - 1
-            
+
+            window[s[i-1]] = window.get(s[i-1], 0) - 1
+            if window.get(s[i-1], 0) == 0:
+                del window[s[i-1]]
             window[s[j]] = window.get(s[j], 0) + 1
-            if window.get(s[j]) == requirement.get(s[j]):
+
+            if window.get(s[j], 0) == p_dict.get(s[j], 0):
                 formed += 1
-            if formed == len(requirement):
-                ans.append(i)
+            if formed == requirement:
+                ans.append(i) 
             j += 1
             i += 1
-            
+
         return ans
