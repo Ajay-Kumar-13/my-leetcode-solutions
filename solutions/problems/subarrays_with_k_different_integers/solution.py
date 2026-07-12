9a84db22f16e17bea5937538a9fdcc0a310af01c
Ajay-Kumar-13 <apulleti@gitam.in>
2026-07-12 16:56:00 +0000

    Sync LeetCode submission - Subarrays with K Different Integers (python)

--- solutions/problems/subarrays_with_k_different_integers/solution.py
+++ solutions/problems/subarrays_with_k_different_integers/solution.py
@@ -5,27 +5,26 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        
         def atMost(k):
-
             frequency = {}
-            j = 0
-            i = 0
             total = 0
-            
-            while j < len(nums):
-                
-                frequency[nums[j]] = frequency.get(nums[j], 0) + 1
+
+            i = 0
+
+            for j in range(len(nums)):
+                frequency[nums[j]] = frequency.get(nums[j], 0)+1
 
                 while len(frequency) > k:
-                    frequency[nums[i]] = frequency.get(nums[i], 0) - 1
+                    frequency[nums[i]] = frequency.get(nums[i], 0)-1
                     if frequency.get(nums[i]) == 0:
                         del frequency[nums[i]]
                     i += 1
 
-                total += (j - i + 1)
-                
-                j += 1
-                
+                if len(frequency) <= k:
+                    total += j-i+1
+
             return total
-            
+
         return atMost(k) - atMost(k-1)
+            
