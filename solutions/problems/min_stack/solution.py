b74b84db687455c4f35581fdbdb3066bfb662590
Ajay-Kumar-13 <apulleti@gitam.in>
2026-07-25 10:48:26 +0000

    Sync LeetCode submission - Min Stack (python)

--- solutions/problems/min_stack/solution.py
+++ solutions/problems/min_stack/solution.py
@@ -2,47 +2,34 @@ class MinStack(object):
 
     def __init__(self):
         self.stack = []
-        self.topEle = -1
-        self.auxiliary_stack = []
-        
 
     def push(self, value):
         """
         :type value: int
         :rtype: None
         """
-        self.stack.append(value)
-        if len(self.auxiliary_stack) == 0:
-            self.auxiliary_stack.append(value)
+        if len(self.stack) > 0:
+            self.stack.append((value, min(value, self.stack[-1][1])))
         else:
-            top = self.auxiliary_stack[self.topEle]
-            if value > top:
-                self.auxiliary_stack.append(top)
-            else:
-                self.auxiliary_stack.append(value)
-        self.topEle += 1
-
+            self.stack.append((value, value))
+        
     def pop(self):
         """
         :rtype: None
         """
         self.stack.pop()
-        self.auxiliary_stack.pop()
-        self.topEle -= 1
 
     def top(self):
         """
         :rtype: int
         """
-        return self.stack[self.topEle]
+        return self.stack[-1][0]
 
     def getMin(self):
         """
         :rtype: int
         """
-        return self.auxiliary_stack[self.topEle]
-        
-        
+        return self.stack[-1][1]
 
 
 # Your MinStack object will be instantiated and called as such:
