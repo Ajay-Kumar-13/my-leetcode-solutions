29eef108f328546d985ab63aded16624f38bfe55
Ajay-Kumar-13 <apulleti@gitam.in>
2026-07-25 06:26:44 +0000

    Sync LeetCode submission - Valid Perfect Square (python)

--- solutions/problems/valid_perfect_square/solution.py
+++ solutions/problems/valid_perfect_square/solution.py
@@ -5,16 +5,19 @@ class Solution(object):
         :rtype: bool
         """
         
-        i = 1 
+        i = 1
         j = num
 
         while i <= j:
-            mid = (i+j) // 2
-            if mid * mid == num:
+            mid = (i+j)//2
+
+            n = mid*mid
+
+            if n == num:
                 return True
-            elif mid * mid > num:
+            elif n > num:
                 j = mid - 1
             else:
                 i = mid + 1
-        
+
         return False
