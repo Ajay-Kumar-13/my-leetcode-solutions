3c822d4dbe177d6efd64c62de8c6039d9b9eeed8
Ajay-Kumar-13 <apulleti@gitam.in>
2026-07-25 04:03:07 +0000

    Sync LeetCode submission - Pacific Atlantic Water Flow (python)

--- solutions/problems/pacific_atlantic_water_flow/solution.py
+++ solutions/problems/pacific_atlantic_water_flow/solution.py
@@ -6,52 +6,45 @@ class Solution(object):
         :type heights: List[List[int]]
         :rtype: List[List[int]]
         """
-                
+
         m = len(heights)
         n = len(heights[0])
-
+        
         pacific = deque([])
         atlantic = deque([])
+
 
         for i in range(n):
             pacific.append((0, i))
 
-        for i in range(1,m):
+        for i in range(1, m):
             pacific.append((i, 0))
 
-        for i in range(n):
-            atlantic.append((m-1, i))
-
-        for i in range(m-1):
+        for i in range(m):
             atlantic.append((i, n-1))
 
+        for i in range(n-1):
+            atlantic.append((m-1, i))
+
         def findPath(q):
-            
-            visited = set()
-            found_way_to_pacific = False
-            found_way_to_atlanctic = False
+            visited =set(q)
 
             while len(q) > 0:
-                
                 x, y = q.popleft()
-
-                visited.add((x,y))
                 
-                current_height = heights[x][y]
-
-                if x > 0 and current_height <= heights[x-1][y] and (x-1, y) not in visited:
-                    q.append((x-1, y))
-                    visited.add((x-1, y))
-                if x < m-1 and current_height <= heights[x+1][y] and (x+1, y) not in visited:
-                    q.append((x+1, y))
-                    visited.add((x+1, y))
-                if y < n-1 and current_height <= heights[x][y+1] and (x, y+1) not in visited:
+                if y < n-1 and heights[x][y] <= heights[x][y+1] and (x, y+1) not in visited:
                     q.append((x, y+1))
                     visited.add((x, y+1))
-                if y > 0 and current_height <= heights[x][y-1] and (x, y-1) not in visited:
+                if y > 0 and heights[x][y] <= heights[x][y-1] and (x, y-1) not in visited:
                     q.append((x, y-1))
                     visited.add((x, y-1))
-            
+                if x < m-1 and heights[x][y] <= heights[x+1][y] and (x+1, y) not in visited:
+                    q.append((x+1, y))
+                    visited.add((x+1, y))
+                if x > 0 and heights[x][y] <= heights[x-1][y] and (x-1, y) not in visited:
+                    q.append((x-1, y))
+                    visited.add((x-1, y))
+    
             return visited
 
         return list(findPath(pacific).intersection(findPath(atlantic)))
