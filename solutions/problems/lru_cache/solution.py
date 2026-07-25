d43e597e14957e17d29baa70a80164fb56561ded
Ajay-Kumar-13 <apulleti@gitam.in>
2026-07-25 14:18:19 +0000

    Sync LeetCode submission - LRU Cache (python)

--- solutions/problems/lru_cache/solution.py
+++ solutions/problems/lru_cache/solution.py
@@ -1,88 +1,90 @@
 class Node:
-    def __init__(self,key=0, val=0, prev=None, next=None):
+    def __init__(self, key, val, prev=None, next=None):
+        self.key = key
         self.val = val
         self.prev = prev
-        self.key = key
         self.next = next
-    
-    def insertBeforeHead(self, key, value, head):
-        existingChain = head.next
-        newNode = Node(key, value)
-        head.next = newNode
-        newNode.prev = head
-        
-        newNode.next = existingChain
-        if existingChain:
-            existingChain.prev = newNode
-        
-        return newNode
-        
-    def makeHead(self, node, head):
-        
-        # If node is head
-        if node.prev == head:
-            return
-        
-        existingChain = head.next
-        
-        # will take care of before and after nodes
-        node.next.prev = node.prev
-        node.prev.next = node.next
-        
-        # Since the current node is a head
-        node.prev = head
-        
-        # making it a head
-        head.next = node
-        
-        # link the chain to the new head
-        node.next = existingChain
-        existingChain.prev = node
-                
-    def removeTail(self, node):
-        if not node:
-            return 
-        if node.prev:
-            node.prev.next = node.next
-        node.next.prev = node.prev
-        
-        node.next = None
-        node.prev = None
-        
-        return node
-        
+
 class LRUCache(object):
 
     def __init__(self, capacity):
         """
         :type capacity: int
         """
-        self.capacity = capacity
         self.cache = {}
-        self.memory = Node(0,0)
-        self.tail = Node(0,0)
-        
-        self.memory.next = self.tail
-        self.tail.prev = self.memory
+        self.capacity = capacity
+
+        self.dummyHead = Node(-1, -1)
+        self.dummyTail = Node(-1, -1)
+
+        self.dummyHead.next = self.dummyTail
+        self.dummyTail.prev = self.dummyHead
+
+    def adjustChain(self, node):
+
+        # link prev node of the current node to the next node
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+        head = self.dummyHead.next
+
+        # Link the new node to the dummy as head
+        self.dummyHead.next = node
+        node.prev = self.dummyHead
+
+        # link the existing chain to the new head
+        node.next = head
+        head.prev = node
 
     def get(self, key):
-        
-        target_node = self.cache.get(key, -1)
-        if target_node != -1:
-            self.memory.makeHead(target_node, self.memory)
-            return target_node.val
+        """
+        :type key: int
+        :rtype: int
+        """
+        node = self.cache.get(key, None)
+
+        if node is not None:
+            self.adjustChain(node)
+            return node.val
         else:
             return -1
 
     def put(self, key, value):
-        existingNode = self.cache.get(key, None)
-        if existingNode:
-            existingNode.val = value
-            self.memory.makeHead(existingNode, self.memory)
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        # Removes the least used node in the linked list
+        def removeLeastUsedNode():
+            del self.cache[self.dummyTail.prev.key]
+
+            tail = self.dummyTail.prev.prev
+
+            # update the tail, delete the last node(original tail)
+            tail.next = self.dummyTail
+            self.dummyTail.prev = tail
+
+        node = self.cache.get(key, None)
+
+        if node is not None:
+            node.val = value
+
+            # link prev node of the current node to the next node
+            node.prev.next = node.next
+            node.next.prev = node.prev
         else:
+            node = Node(key, value)
             if len(self.cache) >= self.capacity:
-                LRU = self.memory.removeTail(self.tail.prev)
-                if LRU:
-                    del self.cache[LRU.key]
-            node = self.memory.insertBeforeHead(key, value, self.memory)
+                removeLeastUsedNode()
             self.cache[key] = node
+
+        head = self.dummyHead.next
+
+        # Link the new node to the dummy as head
+        self.dummyHead.next = node
+        node.prev = self.dummyHead
+
+        # link the existing chain to the new head
+        node.next = head
+        head.prev = node
