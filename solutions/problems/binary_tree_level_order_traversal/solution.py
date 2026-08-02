0d8be51289431b85fabe415070e6bcf9d0936e0d
Ajay-Kumar-13 <apulleti@gitam.in>
2026-08-02 12:40:44 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (python3)

--- solutions/problems/binary_tree_level_order_traversal/solution.py
+++ solutions/problems/binary_tree_level_order_traversal/solution.py
@@ -1,32 +1,35 @@
+from collections import deque
+
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-
-    def getLevelOrder(self, root, level, allElements):
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
 
         if not root:
-            return allElements
+            return []
+        
+        q = deque([root])
 
-        if level+1 > len(allElements):
-            allElements.append([])
+        ans = []
 
-        allElements[level].append(root.val)
+        while len(q) > 0:
+            level = []
 
-        allElements = self.getLevelOrder(root.left, level+1, allElements)
+            for i in range(len(q)):
+                node = q.popleft()
 
-        allElements = self.getLevelOrder(root.right, level+1, allElements)
+                level.append(node.val)
 
-        return allElements
+                if node.left is not None:
+                    q.append(node.left)
+                if node.right is not None:
+                    q.append(node.right)
 
+            ans.append(level)
 
-    def levelOrder(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: List[List[int]]
-        """
-
-        return self.getLevelOrder(root, 0, [])
+        return ans
+            
