fbcd2c01359f5ff5afedf1dbabe816203ef52644
Ajay-Kumar-13 <apulleti@gitam.in>
2026-08-02 12:18:07 +0000

    Sync LeetCode submission - Diameter of Binary Tree (python3)

--- solutions/problems/diameter_of_binary_tree/solution.py
+++ solutions/problems/diameter_of_binary_tree/solution.py
@@ -1,39 +1,23 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def __init__(self):
-        self.diameter = 0
+class Solution:
 
-    def getDiameter(self, root, count):
-        if not root.left and not root.right:
-            return (count)
+    def diameter(self, root, count, maxPath):
+
+        if root is None:
+            return (0, maxPath)
+
+        left, maxPath = self.diameter(root.left, count+1, maxPath)
+        right, maxPath = self.diameter(root.right, count+1, maxPath)
+
+        return (max(left+1, right+1), max(maxPath, left+right))
+
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+
+        return self.diameter(root, 0, 0)[1]
+
         
-        maxDepthOfLeftTree = 0
-        if root.left:
-            stats = self.getDiameter(root.left, count+1)
-            maxDepthOfLeftTree = stats - count
-
-        maxDepthOfRightTree = 0
-        if root.right:
-            stats = self.getDiameter(root.right, count+1)
-            maxDepthOfRightTree = stats - count
-
-        self.diameter = max(self.diameter, maxDepthOfLeftTree+maxDepthOfRightTree)
-
-        return max(maxDepthOfLeftTree, maxDepthOfRightTree) + count
-
-    def diameterOfBinaryTree(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: int
-        """
-        if not root:
-            return 0
-
-        self.getDiameter(root, 0)
-
-        return self.diameter
