57bdc34791c22c719c25162676cad953d7bd8603
Ajay-Kumar-13 <apulleti@gitam.in>
2026-08-02 05:22:45 +0000

    Sync LeetCode submission - Online Stock Span (python3)

--- solutions/problems/online_stock_span/solution.py
+++ solutions/problems/online_stock_span/solution.py
@@ -1,24 +1,19 @@
-class StockSpanner(object):
+class StockSpanner:
 
     def __init__(self):
-    
-        self.stack = []
-        
-    def next(self, price):
-        """
-        :type price: int
-        :rtype: int
-        """
-        ans = 1
-        while len(self.stack) > 0 and price >= self.stack[-1][0]:
-            obj = self.stack.pop()
-            ans += obj[1]
-            
-        self.stack.append((price, ans))
-        
-        return ans
-        
+        self.stock = []
 
+    def next(self, price: int) -> int:
+        
+        span = 1
+        while len(self.stock) > 0 and price >= self.stock[-1][0]:
+            s = self.stock.pop()
+            span += s[1]
+
+        self.stock.append((price, span))
+
+        return span
+        
 
 # Your StockSpanner object will be instantiated and called as such:
 # obj = StockSpanner()
