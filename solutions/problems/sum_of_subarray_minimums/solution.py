1ca505f8a111dc7cbb4caa315f3a01cc74a0dcdf
Ajay-Kumar-13 <apulleti@gitam.in>
2026-08-04 03:25:18 +0000

    Sync LeetCode submission - Sum of Subarray Minimums (python3)

--- solutions/problems/sum_of_subarray_minimums/solution.py
+++ solutions/problems/sum_of_subarray_minimums/solution.py
@@ -1,39 +1,27 @@
-class Solution(object):
-    def sumSubarrayMins(self, arr):
-        """
-        :type arr: List[int]
-        :rtype: int
-        """
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
         
-        nse = [0]*len(arr)
-        pse = [0]*len(arr)
-        
-        ans = 0
-        
+        l = len(arr)
+
+        NS = [l]*l
+        PS = [-1]*l
+
         stack = []
-        for i in range(len(arr)):
+        for i in range(l):
             while len(stack) > 0 and arr[stack[-1]] >= arr[i]:
-                nse[stack[-1]] = i - stack[-1] - 1
+                NS[stack[-1]] = i
                 stack.pop()
             stack.append(i)
             
-        for ind in stack:
-            nse[ind] = len(arr)-1 -ind
-            
-        stack = []
-        for i in range(len(arr)-1, -1, -1):
+        stack=[]
+        for i in range(l-1, -1, -1):
             while len(stack) > 0 and arr[stack[-1]] > arr[i]:
-                pse[stack[-1]] = stack[-1] - i - 1
+                PS[stack[-1]] = i
                 stack.pop()
             stack.append(i)
             
-        for ind in stack:
-            pse[ind] = ind - 0
-            
-        for i in range(len(arr)):
-            ans += ((pse[i]+1) * (nse[i]+1))*arr[i]
-            
-        MOD = 10**9 + 7
-        return ans % MOD
-            
-        return ans
+        count = 0
+        for i in range(l):
+            count += (arr[i]*(NS[i]-i)*(i-PS[i]))
+
+        return count % 1000000007
