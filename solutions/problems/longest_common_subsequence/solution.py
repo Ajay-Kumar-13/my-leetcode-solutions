7520370434c2ff37e813f5c5010c9f2335fc8d98
Ajay-Kumar-13 <apulleti@gitam.in>
2026-08-06 13:07:58 +0000

    Sync LeetCode submission - Longest Common Subsequence (python3)

--- solutions/problems/longest_common_subsequence/solution.py
+++ solutions/problems/longest_common_subsequence/solution.py
@@ -1,17 +1,22 @@
 class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        
-        lt1 = len(text1)+1
-        lt2 = len(text2)+1
 
-        dp = [[0] * (lt2) for _ in range(lt1)]
+    def getLongestLength(self, ind1, ind2, text1, text2, dp):
+
+        if ind1 == len(text1) or ind2 == len(text2):
+            return 0
+        elif dp[ind1][ind2] != -1:
+            return dp[ind1][ind2]
+
         
+        if text1[ind1] == text2[ind2]:
+            dp[ind1][ind2] = 1 + self.getLongestLength(ind1+1, ind2+1, text1, text2, dp)
+            return dp[ind1][ind2]
+        else:
+            dp[ind1][ind2] =  max(self.getLongestLength(ind1+1, ind2, text1, text2, dp), self.getLongestLength(ind1, ind2+1, text1, text2, dp))
+            return dp[ind1][ind2]
+
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+
+        dp = [[-1]*len(text2) for _ in range(len(text1))]
         
-        for i in range(1, lt1):
-            for j in range(1, lt2):
-                if text1[i-1] == text2[j-1]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
-                else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-                    
-        return dp[lt1-1][lt2-1]
+        return self.getLongestLength(0, 0, text1, text2, dp)
