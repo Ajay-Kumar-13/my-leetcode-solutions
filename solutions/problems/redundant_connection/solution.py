3f76f6f06b61ba1ec37817c2ba889cf838bc2fda
Ajay-Kumar-13 <apulleti@gitam.in>
2026-08-08 19:45:44 +0000

    Sync LeetCode submission - Redundant Connection (python3)

--- solutions/problems/redundant_connection/solution.py
+++ solutions/problems/redundant_connection/solution.py
@@ -1,31 +1,22 @@
-class Solution(object):
-    def findRedundantConnection(self, edges):
-        """
-        :type edges: List[List[int]]
-        :rtype: List[int]
-        """
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
 
         roots = {}
-
-        # DSU - Disjoint Set Union
-
-        def findRoot(x):
-            while x != roots.get(x):
-                x = roots.get(x)
-            return x
-        
-        for i in range(1,len(edges)+1):
+        for i in range(1, len(edges)+1):
             roots[i] = i
 
+        def findRoot(r):
+            while roots.get(r) != r:
+                r = roots.get(r)
+
+            return r
+
+
         for edge in edges:
-            
-            x = edge[0]
-            y = edge[1]
+            x = findRoot(edge[0])
+            y = findRoot(edge[1])
 
-            xRoot = findRoot(x)
-            yRoot = findRoot(y)
-
-            if xRoot == yRoot:
+            if x != y:
+                roots[x] = y
+            else:
                 return edge
-
-            roots[xRoot] = yRoot
