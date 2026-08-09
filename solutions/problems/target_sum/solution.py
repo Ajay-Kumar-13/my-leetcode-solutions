289306adbe7525114e10a273942d84fd62b51506
Ajay-Kumar-13 <apulleti@gitam.in>
2026-08-09 17:50:59 +0000

    Sync LeetCode submission - Target Sum (python3)

--- solutions/problems/target_sum/solution.py
+++ solutions/problems/target_sum/solution.py
@@ -1,60 +1,32 @@
 class Solution:
-
-    def findWays(self, ind, target, nums, dp):
-
-        if dp[ind][target] != -1:
-            return dp[ind][target]
-
-        if ind == 0:
-            if target == 0 and nums[0] == 0:
-                dp[ind][target] = 2
-                return 2
-            if target == nums[ind] or target == 0:
-                dp[ind][target] = 1
-                return 1
-            return 0 
-
-        notPick = self.findWays(ind-1, target, nums, dp)
-        pick = 0
-        if (nums[ind] <= target):
-            pick = self.findWays(ind-1, target-nums[ind], nums, dp)
-
-        dp[ind][target] = notPick+pick
-        return notPick+pick
-
-
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+
+        def findWays(ind, target, dp):
+            if ind == len(nums):
+                if target == 0:
+                    return 1
+                return 0
+
+            if dp[ind][target] != -1:
+                return dp[ind][target]
+
+            pick = 0
+            if nums[ind] <= target:
+                pick = findWays(ind+1, target-nums[ind], dp)
+            
+            notPick = findWays(ind+1, target, dp)
+            
+            dp[ind][target] = pick+notPick
+
+            return pick+notPick
 
         diff = sum(nums)-target
 
-        target = diff//2
-
-        if diff < 0 or diff % 2 != 0:
+        if diff % 2 != 0 or diff < 0 or sum(nums) < target:
             return 0
 
-        l = len(nums)
+        target = (diff)//2
 
-        dp = [[0]*(target+1) for _ in range(l)]
+        dp = [[-1]*(target+1) for _ in range(len(nums))]
 
-        for i in range(l):
-            dp[i][0] = 1
-
-        if nums[0] == 0:
-            dp[0][0] = 2
-        else:
-            dp[0][0] = 1
-
-        if nums[0] != 0 and nums[0] <= target:
-            dp[0][nums[0]] = 1
-
-        for ind in range(1, l):
-            for j in range(target+1):
-
-                notPick = dp[ind-1][j]
-                pick = 0
-                if (nums[ind] <= j):
-                    pick = dp[ind-1][j-nums[ind]]
-
-                dp[ind][j] = notPick+pick
-        
-        return dp[l-1][target]
+        return findWays(0, target, dp)
