02b638275bb9abb91f7ab35d67696a0a06e6b376
Ajay-Kumar-13 <apulleti@gitam.in>
2026-08-09 13:15:29 +0000

    Sync LeetCode submission - Coin Change (python3)

--- solutions/problems/coin_change/solution.py
+++ solutions/problems/coin_change/solution.py
@@ -1,34 +1,34 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
 
-        coinsdict = {}
-        
-        def bestOf(amount):
-            if amount <= 0:
+        dp = [[-1]*(amount+1) for _ in range(len(coins))]
+
+        def minimumCoins(ind, coins, amount):
+    
+            if ind == len(coins):
+                return float('inf')
+            
+            if amount == 0:
                 return 0
 
-            if coinsdict.get(amount, None) is not None:
-                return coinsdict.get(amount)
+            if dp[ind][amount] != -1:
+                return dp[ind][amount]
                 
-            dp = []
+            balance = amount - coins[ind]
+                
+            take = float('inf')
+            if balance >= 0:
+                take = 1 + minimumCoins(ind, coins, balance)
             
-            for c in coins:
-                balance = amount - c
-                if coinsdict.get(balance, None) is not None and coinsdict.get(balance, None) != -1:
-                    dp.append(1+ coinsdict.get(balance))
-                elif balance == 0:
-                    dp.append(1)
-                elif balance > 0:
-                    best = bestOf(balance)
-                    if best != -1:
-                        minimumCoins = 1+best
-                        dp.append(minimumCoins)
+            notTake = minimumCoins(ind+1, coins, amount)
 
-            if dp:
-                coinsdict[amount] = min(dp)
-                return coinsdict.get(amount)
-            else:
-                coinsdict[amount] = -1
-                return -1
+            dp[ind][amount] = min(take, notTake)
+            
+            return min(take, notTake)
 
-        return bestOf(amount)
+        totalCoins = minimumCoins(0, coins, amount)
+        
+        if totalCoins != float('inf'):
+            return totalCoins
+
+        return -1
