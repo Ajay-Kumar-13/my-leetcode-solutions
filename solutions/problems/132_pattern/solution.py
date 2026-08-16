c0cb1a2e77fc75eb127d128ec632c1e32b562cff
Ajay-Kumar-13 <apulleti@gitam.in>
2026-08-16 13:08:43 +0000

    Sync LeetCode submission - 132 Pattern (python3)

--- solutions/problems/132_pattern/solution.py
+++ solutions/problems/132_pattern/solution.py
@@ -1,24 +1,17 @@
-class Solution(object):
-    def find132pattern(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+
         stack = []
+        n = len(nums)
+        two = -float('inf')
+        for i in range(n-1, -1, -1):
 
-        medium_right = -float('inf')
-
-        for i in range(len(nums)-1,-1,-1):
-            val = nums[i]
-
-            while len(stack) > 0 and stack[-1] < val:
-                medium_right = max(medium_right, stack[-1])
-                stack.pop()
-
-            if val < medium_right:
+            if nums[i] < two:
                 return True
+            
+            while len(stack) > 0 and stack[-1] < nums[i]:
+                two = max(two, stack.pop())
 
-            stack.append(val)
-        
+            stack.append(nums[i])
+
         return False
-        
