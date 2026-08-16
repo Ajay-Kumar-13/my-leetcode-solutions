b4bb5989337f702d39262d431f587f9f0d864777
Ajay-Kumar-13 <apulleti@gitam.in>
2026-08-16 16:51:51 +0000

    Sync LeetCode submission - House Robber IV (python3)

--- solutions/problems/house_robber_iv/solution.py
+++ solutions/problems/house_robber_iv/solution.py
@@ -1,37 +1,31 @@
-class Solution(object):
+class Solution:
     def minCapability(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        
-        def isPossible(nums, mid, k):
-            i = 0
-            while i < len(nums):
-                if nums[i] <= mid:
-                    k -= 1
-                    i += 2
-                else:
-                    i += 1
-                    
-            if k <= 0:
-                return True
-                
-            return False
         
         i = min(nums)
         j = max(nums)
+
+        def possible(val):
+            count = 0
+            i = 0
+            while i < len(nums):
+                if nums[i] <= val:
+                    count += 1
+                    i += 2
+                    continue
+                i += 1
+
+            if count >= k:
+                return True
+
+            return False
         
-        minCapability = float('inf')
-        
+        ans = float('inf')
         while i <= j:
-            mid = (i + j) // 2
-            
-            if isPossible(nums, mid, k):
-                minCapability = min(minCapability, mid)
+            mid = (i+j) // 2
+            if possible(mid):
+                ans = min(ans, mid)
                 j = mid - 1
             else:
                 i = mid + 1
-                
-        return minCapability
+
+        return ans
