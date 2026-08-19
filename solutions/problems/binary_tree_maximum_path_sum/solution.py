e9d8278dd210015abde44c6369f5947a06fd10e0
Ajay-Kumar-13 <apulleti@gitam.in>
2026-08-19 06:49:33 +0000

    Sync LeetCode submission - Binary Tree Maximum Path Sum (python3)

--- solutions/problems/binary_tree_maximum_path_sum/solution.py
+++ solutions/problems/binary_tree_maximum_path_sum/solution.py
@@ -1,32 +1,20 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution(object):
-
-    maxSum = -float('inf')
-
-    def findMaxPathSum(self, root):
-        if not root:
-            return 0
-
-        leftSum = self.findMaxPathSum(root.left)
-
-        rightSum = self.findMaxPathSum(root.right)
-
-        maxInNode = max(root.val+leftSum, root.val+rightSum, root.val+leftSum+rightSum, root.val)
-        
-        self.maxSum = max(self.maxSum, maxInNode)
-
-        return max(root.val+leftSum, root.val+rightSum, root.val)
-
-
+class Solution:
     def maxPathSum(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: int
-        """
-        self.findMaxPathSum(root)
-        return self.maxSum
+
+        def getMaxPath(root, ans):
+
+            if root is None:
+                return -float('inf'), ans
+
+            L, ans = getMaxPath(root.left, ans)
+            
+            R, ans = getMaxPath(root.right, ans)
+            
+            value = max(L+root.val, R+root.val, root.val)
+            
+            ans = max(value, ans, L+R+root.val)
+            
+            return value, ans
+        
+        return getMaxPath(root, -float('inf'))[1]
+        
