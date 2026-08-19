ea2fbb99dd50b7eef562130cc53ca769b8d28811
Ajay-Kumar-13 <apulleti@gitam.in>
2026-08-19 12:13:29 +0000

    Sync LeetCode submission - Serialize and Deserialize Binary Tree (python3)

--- solutions/problems/serialize_and_deserialize_binary_tree/solution.py
+++ solutions/problems/serialize_and_deserialize_binary_tree/solution.py
@@ -1,90 +1,55 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-import re 
+import re
 
 class Codec:
-
-    def getTreeInFormOfString(self, root, ans):
-        if not root:
-            return ans
-
-        if root.left is None and root.right is None:
-            ans += str(root.val)
-            return ans
-
-        ans += str(root.val) + '('
-
-        ans = self.getTreeInFormOfString(root.left, ans)
+    
+    def convertToString(self, root, ans):
         
-        ans += ")("
-
-        ans = self.getTreeInFormOfString(root.right, ans)
+        if root is None:
+            return ans
+        
+        ans += (str(root.val) + '(')
+        
+        ans = self.convertToString(root.left, ans)
+        
+        ans += ')('
+        
+        ans = self.convertToString(root.right, ans)
         
         if ans[-1] == "(":
-            ans = ans[:len(ans)-1]
+            ans = ans[:-1]
         else:
-            ans += ')'
-
-
+            ans += ")"
+        
         return ans
         
-
     def serialize(self, root):
-        """Encodes a tree to a single string.
-        
-        :type root: TreeNode
-        :rtype: str
-        """
-        strFormat = self.getTreeInFormOfString(root, "")
-        
-        print(strFormat, "after serialization")
-
-        return strFormat
-
+        return self.convertToString(root, "")        
 
     def deserialize(self, data):
-        """Decodes your encoded data to tree.
-        
-        :type data: str
-        :rtype: TreeNode
-        """
-
-        def isInteger(s):
-            # Strip whitespace and check if the remaining string is an int
-            s = s.strip()
-            if not s:
-                return False
-            if s[0] in ('-', '+'):
-                return s[1:].isdigit()
-
-            return s.isdigit()
-
-        data = re.findall(r"-\d+|\d+|[()]", data)
-
         if not data:
             return None
-
-        # stack: top element will be the current node
-        currentNode = []
-
-        for i, element in enumerate(data):
-            if element is ")" and data[i-1] != "(":
-                currentNode.pop()
-            elif isInteger(element):
-                node = TreeNode(int(element))
-                if len(currentNode) > 0:
-                    root = currentNode[-1]
-                    if not root.left and data[i-2] == str(root.val):
-                        root.left = node
+            
+        data = re.findall(r"-\d+|\d+|[()]", data)
+        roots = []
+        root = None
+        
+        for element in data:
+            if element is not "(" and element is not ")":
+                newNode = TreeNode(int(element))
+                if not root:
+                    root = newNode
+                if len(roots) > 0:
+                    if roots[-1][0].left is None and not roots[-1][1]:
+                        roots[-1][1] = True
+                        roots[-1][0].left = newNode
                     else:
-                        root.right = node
-
-                    currentNode.append(node)
-                else:
-                    currentNode.append(node) 
-
-        return currentNode[0]
+                        roots[-1][0].right = newNode
+                roots.append([newNode, False])
+                
+            elif element is ")" and roots[-1][1]:
+                roots.pop()
+                
+            elif element is ")":
+                roots[-1][1] = True
+                
+        return root
