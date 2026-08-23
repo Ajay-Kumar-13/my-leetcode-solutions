8f3d5e3e586ca8ccfef2cae3161ce09a56e72833
Ajay-Kumar-13 <apulleti@gitam.in>
2026-08-23 14:48:10 +0000

    Sync LeetCode submission - Shortest Path in Binary Matrix (python3)

--- solutions/problems/shortest_path_in_binary_matrix/solution.py
+++ solutions/problems/shortest_path_in_binary_matrix/solution.py
@@ -1,56 +1,54 @@
 from collections import deque
 
-class Solution(object):
-    def shortestPathBinaryMatrix(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+
+        if grid[0][0] == 1:
+            return -1
         
+        q = deque([(0, 0, 1)])
+
         n = len(grid)
 
-        if grid[0][0] == 1 or grid[n-1][n-1] == 1:
-            return -1
+        visited = set((0, 0))
 
-        q = deque([[0, 0, 1]])
-
-        visited = set((0,0))
-
-        shortest_path = float('inf')
-        
         while len(q) > 0:
-            x, y, path = q.popleft()
-           
-            if x == n-1 and y == n-1:
-                shortest_path = min(shortest_path, path)
 
-            if x < n-1 and y < n-1 and grid[x+1][y+1] == 0 and (x+1, y+1) not in visited:
-                q.append((x+1, y+1, path+1))
-                visited.add((x+1,y+1))
-            if x > 0 and y > 0 and grid[x-1][y-1] == 0 and (x-1, y-1) not in visited:
-                q.append((x-1, y-1, path+1))
-                visited.add((x-1,y-1))
-            if x > 0 and y < n-1 and grid[x-1][y+1] == 0 and (x-1, y+1) not in visited:
-                q.append((x-1, y+1, path+1))
-                visited.add((x-1,y+1))
-            if x < n-1 and y > 0 and grid[x+1][y-1] == 0 and (x+1, y-1) not in visited:
-                q.append((x+1, y-1, path+1))
-                visited.add((x+1,y-1))
+            i, j, path = q.popleft()
 
-            if y < n-1 and grid[x][y+1] == 0 and (x, y+1) not in visited:
-                q.append((x, y+1, path+1))
-                visited.add((x,y+1))
-            if x > 0 and grid[x-1][y] == 0 and (x-1, y) not in visited:
-                q.append((x-1, y, path+1))
-                visited.add((x-1,y))
-            if x < n-1 and grid[x+1][y] == 0 and (x+1, y) not in visited:
-                q.append((x+1, y, path+1))
-                visited.add((x+1,y))
-            if y > 0 and grid[x][y-1] == 0 and (x, y-1) not in visited:  
-                q.append((x, y-1, path+1))
-                visited.add((x,y-1))
+            if i == n-1 and j == n-1:
+                return path
 
-        if shortest_path == float('inf'):
-            return -1
+            if i < n-1 and j < n-1 and grid[i+1][j+1] == 0 and (i+1, j+1) not in visited:
+                visited.add((i+1, j+1))
+                q.append((i+1, j+1, path+1))
 
-        return shortest_path
+            if j < n-1 and grid[i][j+1] == 0 and (i, j+1) not in visited:
+                visited.add((i, j+1))
+                q.append((i, j+1, path+1))
+
+            if i < n-1 and grid[i+1][j] == 0 and (i+1, j) not in visited:
+                visited.add((i+1, j))
+                q.append((i+1, j, path+1))
+
+            if i > 0 and j < n-1 and grid[i-1][j+1] == 0 and (i-1, j+1) not in visited:
+                visited.add((i-1, j+1))
+                q.append((i-1, j+1, path+1))
+
+            if i > 0 and grid[i-1][j] == 0 and (i-1, j) not in visited:
+                visited.add((i-1, j))
+                q.append((i-1, j, path+1))
+
+            if i > 0 and j > 0 and grid[i-1][j-1] == 0 and (i-1, j-1) not in visited:
+                visited.add((i-1, j-1))
+                q.append((i-1, j-1, path+1))
+
+            if j > 0 and grid[i][j-1] == 0 and (i, j+1) not in visited:
+                visited.add((i, j-1))
+                q.append((i, j-1, path+1))
+
+            if i < n-1 and j > 0 and grid[i+1][j-1] == 0 and (i+1, j-1) not in visited:
+                visited.add((i+1, j-1))
+                q.append((i+1, j-1, path+1))
+        
+        return -1
