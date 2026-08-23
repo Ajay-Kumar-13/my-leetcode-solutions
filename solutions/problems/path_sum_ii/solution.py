9f1e29acf6c412f6ab18fd58202457a2347f080d
Ajay-Kumar-13 <apulleti@gitam.in>
2026-08-23 07:46:58 +0000

    Sync LeetCode submission - Path Sum II (python3)

--- solutions/problems/path_sum_ii/solution.py
+++ solutions/problems/path_sum_ii/solution.py
@@ -1,28 +1,27 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def hasPath(self, root, target, path, count, ans):
-    
-        if root is None:
-            return ans
 
-        count += root.val
-        path.append(root.val)
+    def pathSum(self, root, targetSum):
 
-        ans = self.hasPath(root.left, target, path, count, ans)
-        ans = self.hasPath(root.right, target, path, count, ans)
+        ans  = []
 
-        if count == target and root.left is None and root.right is None:
-            ans.append(path[:])
+        def getTargetPathSums(root, target, path):
+            
+            if root is None:
+                return 
 
-        if len(path) > 0:
+            path.append(root.val)
+            
+            if root.left is None and root.right is None:
+                if root.val == target:
+                    ans.append(path[:])
+
+            getTargetPathSums(root.left, target-root.val, path)
+            getTargetPathSums(root.right, target-root.val, path)
+            
             path.pop()
 
+            return path
+        
+        getTargetPathSums(root, targetSum, [])
+        
         return ans
-
-    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
-        return self.hasPath(root, targetSum, [], 0, [])
