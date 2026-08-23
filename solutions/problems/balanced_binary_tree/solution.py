bd0aca77d2cf5162b5c656db5398940c369e3cc5
Ajay-Kumar-13 <apulleti@gitam.in>
2026-08-23 13:58:13 +0000

    Sync LeetCode submission - Balanced Binary Tree (python3)

--- solutions/problems/balanced_binary_tree/solution.py
+++ solutions/problems/balanced_binary_tree/solution.py
@@ -1,48 +1,28 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
 
-    def isHeightBalanced(self, root, height):
+        def getDepth(root):
+            if root is None:
+                return 0, True
+            
+            L, LB = getDepth(root.left)
+            L += 1
+
+            if not LB:
+                return (0, LB)
+
+            R, RB = getDepth(root.right)
+            R += 1
+
+            if not RB:
+                return (0, RB)
+            
+            return (max(L, R), abs(L-R) <= 1)
         
-        isBalanced = True
-        if not root.left and not root.right:
-            return (height, True)
-
-        leftNodeHeight = height
-        if root.left:
-            leftNodeHeight, isBalanced = self.isHeightBalanced(root.left, height+1)
-            if not isBalanced:
-                return (0, False)
-
-        rightNodeHeight = height
-        if root.right:
-            rightNodeHeight, isBalanced = self.isHeightBalanced(root.right, height+1)
-            if not isBalanced:
-                return (0, False)
-
-        balance = rightNodeHeight-leftNodeHeight
-        if balance == 1 or balance == -1 or balance == 0:
-            isBalanced = True
-        else:
-            isBalanced = False
-
-        return (max(rightNodeHeight, leftNodeHeight), isBalanced)
-
-        
-
-    def isBalanced(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: bool
-        """
-        
-        if not root:
-            return True
-
-        height, isBalanced = self.isHeightBalanced(root, 0)
-
-        return isBalanced
+        return getDepth(root)[1]
